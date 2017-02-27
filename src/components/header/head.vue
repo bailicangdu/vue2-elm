ad8c7e568ed639f24c85553dd01eb72e5c7c9d34
maguohua <1264889788@qq.com>
2017-02-27 14:30:31 +0800

    update

--- src/components/header/head.vue
+++ src/components/header/head.vue
@@ -4,7 +4,7 @@
         <slot name='search'></slot>
         <section class="head_goback" v-if="goBack" @click="$router.go(-1)">
             <svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" version="1.1">
-                <polyline points="12,18 4,9 12,0" style="fill:none;stroke:rgb(255,255,255);stroke-width:2">
+                <polyline points="12,18 4,9 12,0" style="fill:none;stroke:rgb(255,255,255);stroke-width:2"/>
             </svg>
         </section>
         <router-link :to="userInfo? '/profile':'/login'" v-if='signinUp' class="head_login">
