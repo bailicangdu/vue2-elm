1d025105e944f110a155ca5172756734af0fd37a
maguohua1 <1264889788@qq.com>
2018-07-19 18:14:01 +0800

    update node-sass

--- build/dev-server.js
+++ build/dev-server.js
@@ -39,7 +39,7 @@ var context = config.dev.context
 switch(process.env.NODE_ENV){
     case 'local': var proxypath = 'http://localhost:8001'; break;
     case 'online': var proxypath = 'http://elm.cangdu.org'; break;
-    default:  var proxypath = config.dev.proxypath; 
+    default:  var proxypath = config.dev.proxypath;
 }
 var options = {
     target: proxypath,
