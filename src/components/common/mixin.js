1161a7da7cc412eb5f0329fee46eb46c81134437
rongxiao-pop <rx@pop-oo.com>
2023-01-09 10:01:38 +0800

    test

--- src/components/common/mixin.js
+++ src/components/common/mixin.js
@@ -4,6 +4,7 @@ import { imgBaseUrl, localapi, proapi } from '../../config/env'
 export const loadMore = {
 	directives: {
 		'load-more': {
+		  
 			bind: (el, binding) => {
 				let windowHeight = window.screen.height;
 				let height;
