adb2310cc6186258be49592ebe462b4bf07938c1
Yuva <94527987+yuvashrikarunakaran@users.noreply.github.com>
2025-01-06 14:06:21 +0530

    Update webpack.dev.conf.js
    The webpack configuration has been updated to include hot-reload setup and improved development tooling

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -1,37 +1,37 @@
-var config = require('../config')
-var webpack = require('webpack')
-var merge = require('webpack-merge')
-var utils = require('./utils')
-var baseWebpackConfig = require('./webpack.base.conf')
-var HtmlWebpackPlugin = require('html-webpack-plugin')
+import path from 'path';
+import webpack from 'webpack';
+import { merge } from 'webpack-merge';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import config from '../config';
+import utils from './utils';
+import baseWebpackConfig from './webpack.base.conf';
 
-// add hot-reload related code to entry chunks
-Object.keys(baseWebpackConfig.entry).forEach(function(name) {
-    baseWebpackConfig.entry[name] = ['./build/dev-client'].concat(baseWebpackConfig.entry[name])
-})
+// Add hot-reload related code to entry chunks
+Object.keys(baseWebpackConfig.entry).forEach((name) => {
+  baseWebpackConfig.entry[name] = ['./build/dev-client', ...baseWebpackConfig.entry[name]];
+});
 
-module.exports = merge(baseWebpackConfig, {
-    module: {
-        loaders: utils.styleLoaders({
-            sourceMap: config.dev.cssSourceMap
-        })
-    },
-    // eval-source-map is faster for development
-    devtool: '#eval-source-map',
-    plugins: [
-        new webpack.DefinePlugin({
-            'process.env': config.dev.env
-        }),
-        // https://github.com/glenjamin/webpack-hot-middleware#installation--usage
-        new webpack.optimize.OccurenceOrderPlugin(),
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoErrorsPlugin(),
-        // https://github.com/ampedandwired/html-webpack-plugin
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            template: 'index.html',
-            favicon: 'favicon.ico',
-            inject: true
-        })
-    ]
-})
+export default merge(baseWebpackConfig, {
+  module: {
+    rules: utils.styleLoaders({
+      sourceMap: config.dev.cssSourceMap,
+    }),
+  },
+  // eval-source-map is faster for development
+  devtool: 'eval-source-map',
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': config.dev.env,
+    }),
+    // Hot Module Replacement plugins
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+    // Generate index.html with correct assets hash for caching
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'index.html',
+      favicon: 'favicon.ico',
+      inject: true,
+    }),
+  ],
+});
